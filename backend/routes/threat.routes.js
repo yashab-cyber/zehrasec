45f1f393cf901e1da9fa53b8da3d02b21352022c
Yashab Alam <77148571+yashab-cyber@users.noreply.github.com>
2025-04-05 14:47:45 +0530

    Update and rename threat.routes.js to authRoutes.js

--- backend/routes/threat.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getThreats , detectAndStoreThreat} from "../controllers/threat.controllers.js";
-
-const router = express.Router();
-
-router.get("/", getThreats);
-router.post("/detect", detectAndStoreThreat);
-
-export default router;
